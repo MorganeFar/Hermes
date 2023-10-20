3c63477f1fe5d3673ee628420718400f2168936c
Morgane <131769763+MorganeFar@users.noreply.github.com>
2023-10-20 10:37:05 +0200

    updates files + add welcome file
    Update : 
    - Welcome menu : can start the game by clicking on the button
    - Player can die by falling
    - If player dies, game over page opens
    - From the game over, player can retry the same level

--- code/global_niveau/gameOver.py
+++ code/global_niveau/gameOver.py
@@ -1,9 +1,9 @@
 # voir pour faire un bouton https://openclassrooms.com/forum/sujet/bouton-cliquable-pygame
 # https://www.codingninjas.com/studio/library/create-the-buttons-in-a-game-using-pygame
 
-from hermesTest2 import pygame, sys, launch
+from mainCode import *
 from sceneryClass import Scenery
-from settings import screen_height, screen_width, replay_button, game_over_logo, charon_pic
+#from settings import screen_height, screen_width, replay_button, game_over_logo, charon_pic
         
 
 pygame.init()
@@ -32,34 +32,33 @@ retry = Scenery(RETRY_POS_X_COEF, RETRY_POS_Y_COEF, RETRY_WIDTH_COEF, RETRY_HEIG
 gameOverGroup.add(retry)
 
 
-
-while True:
-    #Handling input
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #ferme la fenetre/ jeux
-            sys.exit() #arrête tout le programme
+def over():
+    while True:
+        #Handling input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit() #ferme la fenetre/ jeux
+                sys.exit() #arrête tout le programme
             
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            posMouse = pygame.mouse.get_pos()
-            minX = RETRY_POS_X_COEF * screen_width
-            maxX = minX + screen_width * RETRY_WIDTH_COEF
-            minY = RETRY_POS_Y_COEF * screen_height
-            maxY = minY + screen_height * RETRY_HEIGHT_COEF
-            if ( minX <= posMouse[0] <= maxX
-            and minY <= posMouse[1] <= maxY):
-                #lance hermesTest2.py
-                # a relier au bon fichier
-                launch()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                posMouse = pygame.mouse.get_pos()
+                minX = RETRY_POS_X_COEF * screen_width
+                maxX = minX + screen_width * RETRY_WIDTH_COEF
+                minY = RETRY_POS_Y_COEF * screen_height
+                maxY = minY + screen_height * RETRY_HEIGHT_COEF
+                if ( minX <= posMouse[0] <= maxX
+                    and minY <= posMouse[1] <= maxY):
+                    level.isDead = False
+                    start()
             
-    # logic
-    charon.move_left()
-    retry.zoom()
-    charon.update(0.008, 1)
-    retry.update(0.015, 1.9)
-            
-    screen.fill(background)
-    gameOverGroup.draw(screen)
+        # logic
+        charon.move_left()
+        retry.zoom()
+        charon.update(0.008, 1)
+        retry.update(0.015, 1.9)
+                
+        screen.fill(background)
+        gameOverGroup.draw(screen)
     
-    pygame.display.flip()
-    clock.tick(60)# controle la vite de maj
+        pygame.display.flip()
+        clock.tick(60)# controle la vite de maj
