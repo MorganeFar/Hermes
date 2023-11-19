828b144fbc444a0b2d1b03667f902d93a699fad9
ceedjy <145753734+ceedjy@users.noreply.github.com>
2023-11-19 19:20:35 +0100

    Add files via upload

--- code/global_niveau/game_data.py
+++ code/global_niveau/game_data.py
@@ -18,7 +18,8 @@ level_1 = {'terrain': '../../design/niveau1/map/level_1_map_terrain.csv',
            'gravity': 0.8,
            'jump_speed': -16,
            'items': ['arc', 'lyre', 'montre'],
-           'rotation': 0
+           'rotation': 0,
+           'monsters': ['../../design/niveau1/monster/cyclope_w', '../../design/niveau1/monster/satyr', '../../design/niveau1/monster/cow_1', '../../design/niveau1/monster/cow_2']
            } #design/
 
 level_2 = {'terrain': '../../design/niveau2/map/level_2_map_terrain.csv',
@@ -33,9 +34,10 @@ level_2 = {'terrain': '../../design/niveau2/map/level_2_map_terrain.csv',
            'status': 'swim',
            'speed': 4,
            'gravity': 0.04,
-           'jump_speed': -1,
+           'jump_speed': -2,
            'items': ['fourchette', 'peanut butter', 'trident'],
-           'rotation': -90
+           'rotation': -90,
+           'monsters': ['../../design/niveau2/monster/fish', '../../design/niveau2/monster/mermaid', '../../design/niveau2/monster/poulpe']
            }
 
 level_3 = {'terrain': '../../design/niveau3/map/level_3_map_terrain.csv',
@@ -52,7 +54,8 @@ level_3 = {'terrain': '../../design/niveau3/map/level_3_map_terrain.csv',
            'gravity': 0.8,
            'jump_speed': -16,
            'items' : ['livre', 'casque', 'potion'],
-           'rotation':0
+           'rotation':0,
+           'monsters': ['../../design/niveau3/monster/cerber', '../../design/niveau3/monster/ghost', '../../design/niveau3/monster/skeleton']
            }
  
 level_4 = {'terrain': '../../design/niveau1/map/level_1_map_terrain.csv',
@@ -69,7 +72,8 @@ level_4 = {'terrain': '../../design/niveau1/map/level_1_map_terrain.csv',
            'gravity': 0.8,
            'jump_speed': -16,
            'items' : ['ciseaux', 'amour', 'pelle'],
-           'rotation':0
+           'rotation':0,
+           'monsters': ['../../design/niveau4/monster/bat', '../../design/niveau4/monster/fire', '../../design/niveau4/monster/stalactite']
            }
 
 level_5 = {'terrain': '../../design/niveau1/map/level_1_map_terrain.csv',
@@ -86,7 +90,8 @@ level_5 = {'terrain': '../../design/niveau1/map/level_1_map_terrain.csv',
            'gravity': 0.8,
            'jump_speed': -16,
             'items' : [None, None, None], #y'a pas d'item au niveau 5 enfaite,
-            'rotation':0
+            'rotation':0,
+            'monsters': ['../../design/niveau1/monster/cyclope_w', '../../design/niveau1/monster/satyr', '../../design/niveau1/monster/cow_1', '../../design/niveau1/monster/cow_2']
            }
 
 levels = {
