3d4d58b2fc186353cff1d63bd5b9ad2c2ee8d0bc
ceedjy <145753734+ceedjy@users.noreply.github.com>
2023-12-09 15:57:07 +0100

    Update mainCode.py

--- code/global_niveau/mainCode.py
+++ code/global_niveau/mainCode.py
@@ -20,16 +20,23 @@ class Game:
         self.max_level = 1
         self.max_health = 3
         self.cur_health = 3
-        self.item = None  # a voir si on fait comme ca pour les items
+        self.item = None 
         
         # audio
         self.level_bg_music = ['', pygame.mixer.Sound('../../audio/ambiance_niv1.mp3'), pygame.mixer.Sound('../../audio/ambiance_niv2.wav'), pygame.mixer.Sound('../../audio/ambiance_niv3.mp3'), pygame.mixer.Sound('../../audio/ambiance_niv4.mp3'), pygame.mixer.Sound('../../audio/ambiance_niv5.mp3')]
         self.overworld_bg_music = pygame.mixer.Sound('../../audio/overworld.mp3')
         self.dead_sound = pygame.mixer.Sound('../../audio/dead.wav')
+        self.welcome_sound = pygame.mixer.Sound('../../audio/welcome.mp3')
+        self.dialogue_sound = pygame.mixer.Sound('../../audio/dialogue.mp3')
+        self.gameOver_sound = pygame.mixer.Sound('../../audio/gameover.ogg')
         
         # overworld creation
+        self.welcome_sound.play(loops=-1)
         welcome.welcomeMenu()
+        self.welcome_sound.stop()
+        self.dialogue_sound.play(loops=-1)
         maia_dialogue.run()
+        self.dialogue_sound.stop()
         self.overworld = Overworld(1, self.max_level, screen, self.create_level)
         self.status = 'overworld' # le status de ou se trouve le joueur
         self.overworld_bg_music.play(loops=-1)
@@ -47,21 +54,26 @@ class Game:
         self.bg_music = self.level_bg_music[current_level]
         self.bg_music.play(loops=-1)
         self.item = None
-        # self.cur_health = 3 #normalement dès qu'il relance le niveau, donc des que game over
         
     def create_overworld(self, current_level, new_max_level):
         if new_max_level > self.max_level:
             self.max_level = new_max_level
             self.cur_health = 3 
         else:
+            self.bg_music.stop()
             self.dead_sound.play()
+            self.gameOver_sound.play(loops=-1)
             gameOver.over()
+            self.gameOver_sound.stop()
         self.overworld = Overworld(current_level, self.max_level, screen, self.create_level)
         self.status = 'overworld'
         self.bg_music.stop()
+        self.dialogue_sound.stop()
         self.overworld_bg_music.play(loops=-1)
 
     def create_dialogue(self, current_level):
+        self.bg_music.stop()
+        self.dialogue_sound.play(loops=-1)
         self.dialogue = Dialogue(current_level, self.item, self.recom_niveaux[current_level -1])
         self.dialogue.run()
         self.recom_niveaux[current_level -1] = True
@@ -78,9 +90,11 @@ class Game:
             self.cur_health = 3 
             self.item = None 
             self.max_level = 1
-            gameOver.over()
-            self.overworld = Overworld(1, self.max_level, screen, self.create_level)
             self.bg_music.stop()
+            self.gameOver_sound.play(loops=-1)
+            gameOver.over()
+            self.gameOver_sound.stop()
+            self.overworld = Overworld(1, self.max_level, screen, self.create_level)
             self.overworld_bg_music.play(loops=-1)
             self.status = 'overworld'
             
@@ -109,19 +123,3 @@ while True:
 
     pygame.display.update()
     clock.tick(60)
-"""
-def start():
-    while not game.level.isDead : # check si le perso est vivant
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            
-        game.run()
-  
-        pygame.display.update()
-        clock.tick(60)
-    return game.level.isDead
-
-start()
-"""
