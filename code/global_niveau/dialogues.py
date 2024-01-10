d7ce72a265972f3e2787e529bafc14f59892a7dc
ceedjy <145753734+ceedjy@users.noreply.github.com>
2024-01-10 19:24:03 +0100

    Update dialogues.py
    les coms

--- code/global_niveau/dialogues.py
+++ code/global_niveau/dialogues.py
@@ -3,19 +3,21 @@
 les dialogues 
 """
 
+# ---------------- IMPORTATIONS ----------------
 import pygame, sys
 from sceneryClass import Scenery
 from game_data import levels
 from settings import screen_height, screen_width 
+# ----------------------------------------------
 
 class Dialogue:
-    def __init__(self, current_level, objet, recommence): #current_level le level courrant (int), objet l'objet courant (str), et recommence condition si on recommence ou pas (booleen)
-        #global 
+    def __init__(self, current_level, objet, recommence): # Current_level est le level courrant (int), objet est l'objet courant (str), et recommenceest une condition si on recommence ou pas (booleen)
+        # Global 
         self.current_level = current_level
         self.recommence = recommence 
-        self.level_data = levels[self.current_level]
+        self.level_data = levels[self.current_level] # Pour avoir acces au data du niveau 
         
-        #window
+        # Window
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         if current_level != 6 :
             path = "../../design/niveau" + str(self.current_level) + "/background.png"
@@ -25,12 +27,13 @@ class Dialogue:
         self.fond = pygame.transform.scale(self.background, (screen_width, screen_height))
         self.fond = self.fond.convert()
         
-        #les objets 
+        # Les objets 
         self.objet = objet 
-        self.bon_obj = self.level_data['bon_obj']
+        self.bon_obj = self.level_data['bon_obj'] # Le bon objet est l'objet que doit recuperer le joueur pour continuer 
         self.mauv_obj1 = self.level_data['mauv_obj1']
         self.mauv_obj2 = self.level_data['mauv_obj2']
-        
+
+        # Booleens qui permettent de savoir quel objet le joueur a ramasse
         self.objet_bon = False
         self.objet_mauv1 = False
         self.objet_mauv2 = False
@@ -42,12 +45,12 @@ class Dialogue:
         elif self.objet == self.mauv_obj2:
             self.objet_mauv2 = True
         
-        #color
+        # Color
         self.light_grey = (200, 200, 200)
         self.black = (0,0,0)
         self.yellow = (230, 219, 65)
         
-        #Scenery
+        # Mise en place de la scene avec le dieu et Hermes 
         self.place = self.level_data['place_dieu']
         self.olympeGroup = pygame.sprite.Group()
         if self.current_level != 5:
@@ -57,7 +60,7 @@ class Dialogue:
         hermes = Scenery(0.2, 0.40, 0.10, 0.3, ["../../design/hermes/stand/hermes_s.png"])
         self.olympeGroup.add(hermes)
 
-        #contenu des dialogues 
+        # Contenu des dialogues 
         self.dialogue_all = self.level_data['dialogue_all']
         self.dialogue_bon_obj = self.level_data['dialogue_bon_obj']
         self.dialogue_mauv_obj1 = self.level_data['dialogue_mauv_obj1']
@@ -65,59 +68,63 @@ class Dialogue:
         self.dialogue_no = self.level_data['dialogue_no']
         self.dialogue_recom = self.level_data['dialogue_recom']
 
-        #text variables
-        self.game_font = pygame.font.Font("freesansbold.ttf", 25) #le font et la taille du texte 
-        self.zone_text = pygame.Rect(0, screen_height-(screen_height/4), screen_width , screen_height/4) #la zone dans laquelle il y aura le texte 
+        # Variables pour le texte 
+        self.game_font = pygame.font.Font("freesansbold.ttf", 25) # Le font et la taille du texte 
+        self.zone_text = pygame.Rect(0, screen_height-(screen_height/4), screen_width , screen_height/4) # La zone dans laquelle il y aura le texte
+        # Les boutons d'indication des touches pour le joueur 
         self.bouton_suivant = pygame.Rect(screen_width-90, screen_height-50, 70, 30)
         self.bouton_sortir = pygame.Rect(screen_width-180, screen_height-50, 80, 30)
 
-        self.counter = 0 #aide a savoir si on est a la fin du message 
-        self.speed = 2 #on a 1 caract tous les 2 tics 
-        self.active_message = 0 #l indice du message 
+        self.counter = 0 # Aide a savoir si on est a la fin du message 
+        self.speed = 2 # On a 1 caract tous les 2 tics 
+        self.active_message = 0 # L'indice du message 
         self.active_mess_objets = -1
-        self.done = False        
+        self.done = False # Indique si le message est fini    
 
-        if not self.recommence: #le message du debut differe si il recommence me niveau ou non 
+        if not self.recommence: # Le message du debut differe si il recommence le niveau ou non 
             self.message = self.dialogue_all[self.active_message] #le message actuel
         else:
             self.message = self.dialogue_recom[0]
 
         #score timer 
         self.score_time = True
-        
+
+    # Met en place des objets speciaux dans le dialogue du niveau 4
     def objets(self):
         self.trone = Scenery(0.3, 0.4, 0.16, 0.32, ["../../design/niveau4/obj_spe/trone.png"])
         self.olympeGroup.add(self.trone)
         self.shoes = Scenery(0.45, 0.61, 0.12, 0.1, ["../../design/niveau4/obj_spe/chaussure.png"])
         self.olympeGroup.add(self.shoes)
-        
+
+    # Met en place le trone pour le dialogue de fin avec Zeus 
     def chaise(self):
         self.chair = Scenery(0.3, 0.4, 0.16, 0.32, ["../../design/niveau4/obj_spe/trone.png"])
         self.olympeGroup.add(self.chair)
-        
+
+    # Met en place hera dans le dialogue du niveau 5 
     def hera(self):
         self.dieu = Scenery(self.place[0], self.place[1], self.place[2], self.place[3], self.level_data['dieu'])
         self.olympeGroup.add(self.dieu)
 
     def run(self):
-        pygame.init() #always need for any kind of pygame code 
+        pygame.init()
         clock = pygame.time.Clock()
         
-        while True: #the loop 
+        while True:
 
             if self.counter < self.speed * len(self.message):
                 self.counter += 1
             elif self.counter >= self.speed * len(self.message):
                 self.done = True 
             
-            for event in pygame.event.get(): #any movement or intercation or action
+            for event in pygame.event.get(): 
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit() 
                 
-                #si on presse entrer on passe au texte suivant 
+                # Si on presse entrer on passe au texte suivant 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN and self.done and not self.recommence and self.active_message < len(self.dialogue_all) -1: #si il est a son premier coup 
+                    if event.key == pygame.K_RETURN and self.done and not self.recommence and self.active_message < len(self.dialogue_all) -1: # Si le joueur en est a son premier coup 
                         self.active_message += 1
                         self.done = False 
                         self.message = self.dialogue_all[self.active_message]
@@ -127,8 +134,8 @@ class Dialogue:
                         if (self.active_message == 1) and (self.current_level == 5):
                             self.hera()
                     
-                    else: #texte suivant selon l'objet ramasse 
-                        #si le joueur a ramassé le bon objet  
+                    else: # Texte suivant selon l'objet ramasse 
+                        # Si le joueur a ramasse le bon objet  
                         if event.key == pygame.K_RETURN and self.done and self.objet_bon and self.active_mess_objets < len(self.dialogue_bon_obj) -1:
                             self.active_mess_objets += 1
                             self.done = False 
@@ -136,19 +143,19 @@ class Dialogue:
                             self.counter = 0
                             if (self.active_mess_objets == 5) and (self.current_level == 4):
                                 self.objets()
-                        #si le joueur a ramassé le mauvais obj 1
+                        # Si le joueur a ramassé le mauvais obj 1
                         elif event.key == pygame.K_RETURN and self.done and self.objet_mauv1 and self.active_mess_objets < len(self.dialogue_mauv_obj1) -1:
                             self.active_mess_objets += 1
                             self.done = False 
                             self.message = self.dialogue_mauv_obj1[self.active_mess_objets]
                             self.counter = 0
-                        #si le joueur a ramassé le mauvais obj 2
+                        # Si le joueur a ramassé le mauvais obj 2
                         elif event.key == pygame.K_RETURN and self.done and self.objet_mauv2 and self.active_mess_objets < len(self.dialogue_mauv_obj2) -1:
                             self.active_mess_objets += 1
                             self.done = False 
                             self.message = self.dialogue_mauv_obj2[self.active_mess_objets]
                             self.counter = 0
-                        #si le joueur n'a ramassé aucun objet
+                        # Si le joueur n'a ramassé aucun objet
                         elif event.key == pygame.K_RETURN and self.done and (not self.objet_mauv1 and not self.objet_mauv2 and not self.objet_bon) and self.active_mess_objets < len(self.dialogue_no) -1:
                             self.active_mess_objets += 1
                             self.done = False 
@@ -157,12 +164,13 @@ class Dialogue:
                         elif event.key == pygame.K_e:
                             return 'final'
                             
-            #visual 
+            # Visuel
             self.screen.blit(self.fond, (0,-(screen_height/4)))
 
-            #les personnages
+            # Les personnages
             self.olympeGroup.draw(self.screen)
-            #la zone de texte et le bouton suivant
+            
+            # La zone de texte et les boutons
             pygame.draw.rect(self.screen, self.light_grey, self.zone_text)
             pygame.draw.rect(self.screen, self.yellow, self.bouton_suivant)
             pygame.draw.rect(self.screen, self.yellow, self.bouton_sortir)
@@ -172,13 +180,13 @@ class Dialogue:
             self.screen.blit(suivant, (screen_width-80, screen_height-45))
             self.screen.blit(sortir, (screen_width-170, screen_height-45))
             
-            #on cree du texte anime
+            # On cree du texte anime
             self.surface = self.screen 
             text = self.message[0:self.counter//self.speed]
             pos = (screen_width*0.02, screen_height-(screen_height/4) + screen_height*0.02)
             color = 'black'
             
-            #animation de retour a la ligne et de lettre par lettre
+            # Animation de retour a la ligne et de lettre par lettre
             collection = [word.split(' ') for word in text.splitlines()]
             space = self.game_font.size(' ')[0]
             x,y = pos 
@@ -193,8 +201,7 @@ class Dialogue:
                     x += word_width + space 
                 x = pos[0]
                 y += word_height
-            
-            #updating the window 
+                
             pygame.display.flip()
             
             clock.tick(60)
