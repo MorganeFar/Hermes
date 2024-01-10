cb51bb8cfcdf853a063f33b551df6bd9c4083375
ceedjy <145753734+ceedjy@users.noreply.github.com>
2024-01-10 20:29:47 +0100

    Update stalactite.py

--- code/global_niveau/stalactite.py
+++ code/global_niveau/stalactite.py
@@ -3,27 +3,31 @@
 stalactite
 """
 
+# ---------------- IMPORTATIONS ----------------
 import pygame
 from tiles import AnimatedTile
+# ----------------------------------------------
 
 class Stalactite(AnimatedTile):
-    def __init__(self,size,x,y,path,actif,value): #actif est un booleen qui dit si le stalactite bouge ou pas, a voir si on le garde la dedans ou dans le niveau  
+    def __init__(self,size,x,y,path,actif,value): # Actif est un booleen qui dit si le stalactite bouge ou pas 
         super().__init__(size,x,y,path)
-        self.rect.y += size - self.image.get_size()[1] #reposotionne les stalactite 
-        self.speed = 8
-        self.actif = actif
-        self.value = value
-        self.direction = pygame.math.Vector2(0,0)
-        self.gravity = 0.8
+        self.rect.y += size - self.image.get_size()[1] # Reposotionne les stalactite 
+        self.speed = 8 # La vitesse
+        self.actif = actif # Si il es active ou non 
+        self.value = value # Sa valeur, cela va nous indiquer lequl est active 
+        self.direction = pygame.math.Vector2(0,0) # On utilise un vecteur pour la direction 
+        self.gravity = 0.8 # On applique la gravite aux stalactite 
 
+    # Quand il tombe 
     def fall(self):
-        #self.rect.y += self.speed 
         self.direction.y += self.gravity 
         self.rect.y += self.direction.y
-        
+
+    # Il est active 
     def active(self, actif):
         self.actif = actif
 
+    # Ce qui est recalcule pendant le jeu 
     def update(self,shift):
         self.rect.x += shift 
         if self.actif :
