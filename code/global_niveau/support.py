6377bcf64b6dd80fc4e9dc83d01c9e30006b2c6d
ceedjy <145753734+ceedjy@users.noreply.github.com>
2024-01-10 20:33:12 +0100

    Update support.py

--- code/global_niveau/support.py
+++ code/global_niveau/support.py
@@ -3,20 +3,24 @@
 support
 """
 
+# ---------------- IMPORTATIONS ----------------
 import pygame
 from csv import reader 
 from settings import tile_size 
 from os import walk
+# ----------------------------------------------
 
+# Permet d'importer des dossiers 
 def import_folder(path):
     surface_list = []
     for _, __, image_files in walk(path):
-        for image in image_files:  # on a le nom de chaque image qu'il y a dans le dossier du path
+        for image in image_files:  # On a le nom de chaque image qu'il y a dans le dossier du path
             full_path = path + '/' + image 
             image_surf = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surf)
     return surface_list 
 
+# Permet d'importer des fichiers CSV
 def import_csv_layout(path):
     terrain_map = []
     with open(path) as map:
@@ -24,7 +28,8 @@ def import_csv_layout(path):
         for row in level:
             terrain_map.append(list(row))
         return terrain_map 
-    
+
+# Permet d'importer des images qu'on va ensuite recouper pour correspondre aux tiles du niveau 
 def import_cut_graphic(path):
     surface = pygame.image.load(path).convert_alpha()
     tile_num_x = int(surface.get_size()[0] / tile_size) 
